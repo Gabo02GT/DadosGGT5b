a587ea6e047ec35a02bd90e57af655802ca31fe2
Gabriel Garcia Torres <a3522110231@alumno.uttehuacan.edu.mx>
2024-02-28 17:20:43 -0600

    Descripción de los cambios que hiciste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Datos-ggt';
+  title = 'Dados';
+  dadoIzquierdo = '../assets/images/dice1.png';
+  dadoDerecho = '../assets/images/dice4.png';
+  numero1 : number = 1;
+  numero2 : number = 2;
+
+  tirarDados(): void{
+    this.numero1 = Math.round(Math.random() * 5) + 1;	
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+
+    this.dadoIzquierdo = '../assets/images/dice' + this.numero1 + '.png';
+    this.dadoDerecho = '../assets/images/dice' + this.numero2 + '.png';
+
+    //console.log(this.numero1);
+    //console.log(this.numero2);
+  }
 }
---
+++ src/app/components/navbar-ggt/navbar-ggt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ggt-app-navbar-ggt',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-ggt.component.html',
+  styleUrl: './navbar-ggt.component.css'
+})
+export class NavbarGgtComponent {
+
+}
